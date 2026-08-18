f8de5f5db034cc843b8d08cd9d01aebc87bb2bea
HENDRO AGUSTINUS NAIBAHO <6219034421@wil.local>
2026-08-18 17:38:22 +0700

    Test auth route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,6 +2,14 @@ const express = require("express");
 
 const router = express.Router();
 
+router.get("/test", (req, res) => {
+    res.json({
+        succes: true,
+        message: "AUTH ROUTE BERHASIL"
+        
+    });
+});
+
 const {
     login
 } = require("../controllers/authController");
